f9f64aa41cae3ae3c4d4cf2aaf97ef1e6cc77cc4
YeBin_Pi <plixn98@gmail.com>
2025-09-07 15:13:17 +0900

    Update stack_queue.c

--- stack_queue.c
+++ stack_queue.c
@@ -1,13 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
 typedef struct {
     int x, y;
 } Node;
 
 #define MAX 1000
-Node stack[MAX]; 
-int top = 0;   // 스택의 현재 크기 (다음 push 위치)
-
 
 // ******* 스택 *******
+
+Node stack[MAX]; 
+int top = 0;   // 스택의 현재 크기 (다음 push 위치)
 
 // push: 새로운 원소를 스택 위에 추가
 void push(int x, int y) {
@@ -27,9 +31,23 @@ bool empty() {
     return top == 0;
 }
 
+
 // ******* 큐 *******
 
+int queue[MAX * MAX][2];
+int front = 0, rear = 0;
 
+void push(int x, int y) {
+    queue[rear][0] = x;
+    queue[rear][1] = y;
+    rear++;
+}
 
-void push(int x, int y) { queue[rear][0] = x; queue[rear][1] = y; rear++; } 
-Node pop() { Node n; n.x = queue[front][0]; n.y = queue[front][1]; front++; return n; }
+Node pop() {
+    Node n;
+    n.x = queue[front][0];
+    n.y = queue[front][1];
+    front++;
+    return n;
+}
+
